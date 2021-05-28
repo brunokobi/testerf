66010bd59877ec7b1b982316b878c9179600cb29
brunokobi <38856838+brunokobi@users.noreply.github.com>
2021-05-27 23:34:43 -0300

    teste work

--- assets/js/index.js
+++ assets/js/index.js
@@ -40,7 +40,7 @@ function startVideo() {
   const loadLabels = () => {
     // const labels = ['Matheus Castiglioni','Lucas Barbosa','Bruno Kobi','Rafaela Kobi']
 
-    const labels = ['6917100903','6916200116','Lucas','Professor Romulo','Professor Marcelo','Michele','Will','Carol','Augusto','Marcio']
+    const labels = ['Bruno','Lucas','Cloves','Gabriel','Rafaella','Luiza']
     return Promise.all(labels.map(async label => {
         const descriptions = []
         for (let i = 1; i <= 1; i++) {
